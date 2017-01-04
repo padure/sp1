d189a90af79d0275f77e22850693f82bfd69decd
Padure Gheorghe <Padure Gheorghe@DESKTOP-9IO6I24>
2017-01-04 21:04:04 +0200

    galerie

--- resources/views/main/meniu/galerie/activitati.blade.php
+++ resources/views/main/meniu/galerie/activitati.blade.php
@@ -7,31 +7,6 @@
         <img src="{{ asset ( "images/extracurs/img1.jpg" ) }}" class="img-thumbnail"/>
    </a>
     </div>
-   <div class="">
-   <a href="{{ asset ( "images/extracurs/img1.jpg" ) }}" data-title="My first caption" data-lightbox="Vacation">
-        <img src="{{ asset ( "images/extracurs/img1.jpg" ) }}" class="img-thumbnail"/>
-   </a>
-    </div>
-   <div class="">
-   <a href="{{ asset ( "images/extracurs/img1.jpg" ) }}" data-title="My first caption" data-lightbox="Vacation">
-        <img src="{{ asset ( "images/extracurs/img1.jpg" ) }}" class="img-thumbnail"/>
-   </a>
-    </div>
-   <div class="">
-   <a href="{{ asset ( "images/extracurs/img1.jpg" ) }}" data-title="My first caption" data-lightbox="Vacation">
-        <img src="{{ asset ( "images/extracurs/img1.jpg" ) }}" class="img-thumbnail"/>
-   </a>
-    </div>
-   <div class="">
-   <a href="{{ asset ( "images/extracurs/img1.jpg" ) }}" data-title="My first caption" data-lightbox="Vacation">
-        <img src="{{ asset ( "images/extracurs/img1.jpg" ) }}" class="img-thumbnail"/>
-   </a>
-    </div>
-   <div class="">
-   <a href="{{ asset ( "images/extracurs/img1.jpg" ) }}" data-title="My first caption" data-lightbox="Vacation">
-        <img src="{{ asset ( "images/extracurs/img1.jpg" ) }}" class="img-thumbnail"/>
-   </a>
-    </div>
 </div>
 <script src="{{ asset("js/lightbox.min.js") }}"></script>
 <script src="{{ asset("js/lightbox-plus-jquery.min.js") }}"></script>
