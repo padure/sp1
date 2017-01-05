05ce850102575d729b2194c2c6fd1cb5d4d76d07
valintin <valintin@192.168.56.1>
2017-01-05 19:30:03 +0200

    panel

--- resources/views/admin/urna.blade.php
+++ resources/views/admin/urna.blade.php
@@ -1,15 +1,26 @@
 @extends("admin.base")
 @section("content")
-    <h3>Sa stergem fisierele nefolositoare de pe site!</h3>
-    <p class="text-info">Fisierele nefolositoare sunt pozele pe care le incarcati si nu salvati evenimentul , ele doar ocupa memorie!</p>
-    @if(!empty($post["fisierenefolosite"]))
-        <h1>Fisiere nefolositoare: {{$post["fisierenefolosite"]}}</h1>
-        <h1>Memorie folosita de fisiere nefolositoare {{$post["fisierenefolositoaresize"]}} Mb</h1>
-        <a class="btn btn-danger" href="{{URL("admin/deleteurna")}}">
-            <span class="glyphicon glyphicon-remove"></span>
-            Sterge fisierele nefolositoare
-        </a>
-    @else
-        <h1>Fisiere nefolositoare: 0</h1>
-    @endif
+<div class="panel panel-info">
+    <div class="panel-heading">
+        <h3 class="panel-title">Sa stergem fisierele nefolositoare de pe site!</h3>
+    </div>
+    <div class="panel-body">
+        <p class="text-danger">Fisierele nefolositoare sunt pozele pe care le incarcati si nu salvati evenimentul , ele doar ocupa memorie!</p>
+    </div>
+    <div class="panel-footer">
+        @if(!empty($post["fisierenefolosite"]))
+            <h1>Fisiere nefolositoare: {{$post["fisierenefolosite"]}}</h1>
+            <h1>Memorie folosita de fisiere nefolositoare {{$post["fisierenefolositoaresize"]}} Mb</h1>
+            <a class="btn btn-danger" href="{{URL("admin/deleteurna")}}">
+                <span class="glyphicon glyphicon-remove"></span>
+                Sterge fisierele nefolositoare
+            </a>
+        @else
+            <h1>Fisiere nefolositoare: 0</h1>
+        @endif
+        
+    </div>
+</div>
+    
+    
 @endsection
