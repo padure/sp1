050447ce43f052717b1a66b44f2d32e49dfea5a7
Padure Gheorghe <Padure Gheorghe@DESKTOP-9IO6I24>
2017-01-23 19:57:32 +0200

    corp didactic tabel

--- resources/views/main/meniu/despre-noi/corpul.blade.php
+++ resources/views/main/meniu/despre-noi/corpul.blade.php
@@ -1,5 +1,18 @@
 @extends("main.base")
 @section("content")
 <h3>Corpul didactic</h3>
-
+<table class="table table-striped">
+    <tr>
+        <th>NR/Ord</th><th>Nume</th><th>Prenume</th><th>Functia</th>
+    </tr>
+    <tr>
+        <td>1</td><td>Ion</td><td>Ion</td><td>Profesor matematica</td>
+    </tr>
+    <tr>
+        <td>1</td><td>Ion</td><td>Ion</td><td>Profesor matematica</td>
+    </tr>
+    <tr>
+        <td>1</td><td>Ion</td><td>Ion</td><td>Profesor matematica</td>
+    </tr>
+</table>
 @endsection
