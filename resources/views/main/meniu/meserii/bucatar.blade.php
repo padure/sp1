5d668cbb5a82a13d3ac267cffc5737f48904e1e0
Padure Gheorghe <Padure Gheorghe@PC-Gheorghe>
2017-02-07 13:59:39 +0200

    bucatar

--- resources/views/main/meniu/meserii/bucatar.blade.php
+++ resources/views/main/meniu/meserii/bucatar.blade.php
@@ -1,32 +1,61 @@
 @extends("main.base")
 @section("content")
 <div class="col-md-12 meserii whiteclass">
-    <h3>Bucatar/Chelner</h3>
-    <div class="col-md-8 meserii">
-        <p>Programul de formare profesionala la meseria ”Operator Suport Tehnic Calculatoare”,
-are misiunea de a pregati muncitori calificati pentru segmentele pietei muncii din domeniul TIC.</p>
-        <ul>
-            <h4>Operatorul Suport Tehnic Calculatoare este o persoana specializata, care:</h4>
-            <li>Asamblarea si depanarea calculatoarelor personale</li>
-            <li>Intretinerea calculatoarelor personale</li>
-            <li>Instalarea si depanarea retelelor de calculatoare personale</li>
-            <li>Intretinerea retelelor de calculatoare personale</li>
-            <li>Instalarea si configurarea produselor-program</li>
-            <li>Intretinerea produselor-program</li>
-            <li>Crearea unui mediu adecvat de munca si remedierea situatiilor de risc</li>
-        </ul>
+            <h3>Bucatar/Chelner</h3>
+       
+    <div class="col-md-12 meserii">
+        <div class="col-md-8">
+       <b>Ce înveți?</b>
+       <ul>
+           <li>Realizezi preparate culinare din bucătărianațională și internațională </li>
+           <li>Utilizezi tehnologii specifice pentrurealizarea preparatelor dietetice, Utilizezi tehnologii specifice pentru realizarea preparatelor dietetice,   slow-food,fast-food </li>
+           <li>Prepari meniuri pentru servicii de tip catering Aplici tehnici de prezentare estetica a preparatelor culinare</li>
+       </ul>
+       <b>Care sunt beneficiile tale?</b>
+       <ul>
+           <li>Efectuezi stagii de pregătire practica in restaurante</li>
+           <li>Poți obține,ulterior,calificări de bucătar-șef,bucătar</li>
+           <li>specialist,maestru in arta culinară,director de restaurant</li>
+           <li>Este in trend competiția culinara,iar tu vei fi specialist</li>
+       </ul>
+       <b>Ce job poți avea?</b>
+       <ul>
+           <li>Bucatar in restaurant sau firma de catering</li>
+           <li>Lucrător în fast-food,pizzerie Bufetier</li>
+       </ul>
+        </div>
+        <div class="col-md-4">
+            <img style="width: 100%;" src="{{asset('images/meserii/bucatari2.jpg')}}" class="img-thumbnail">
+        </div>
+        </div>
+       <div class="col-md-12">
+           <div class="col-md-4">
+               <img style="width: 100%;" src="{{asset('images/meserii/bucatari1.jpg')}}" class="img-thumbnail" />
+           </div>
+               <div class="col-md-8" style="">
+                <b>Posibili angajatori</b>
+                <ul>
+                    <li>SRL "Schif"(Balcan)</li>
+                    <li>SA  "Modernu"</li>
+                    <li>SA  "Doina Prutului"</li>
+                    <li>SRL "Raffaello"</li>
+                    <li>Îi "Prutcomagro"(Codreanu)</li>
+                    <li>ÎI "Prometeu"</li>
+                </ul>
+           </div>
+       </div>
+<div class="col-md-12">
+    <div class="col-md-8">
+       <b style="margin-top: 35px;">Impresii ale absolvenților</b>
+       <p>" Am invatat sa combin incredientele alimentare in cele mai variate moduri și sper ca in cel mai
+        in cele mai variate moduri și sper ca in cel mai scurt timp sa devin un bucatar desăvarșit."</p>
+       <p>"Am obținut un beneficiu pe viață: am învațat să mănînc sănătos."</p>
+       <p>"Știu acuma sa aleg mai ușor hrana sănătoasă. Citesc mult mai mult despre un stil de viață sănătos,
+           despre restaurantele care se preocup primul rand de sănatate clienților."</p>
     </div>
-    <div class="col-lg-4">
-    <img src="{{ asset ( "images/mager.jpg" ) }}" class="img-thumbnail">
+    <div class="col-md-4">
+            <img style="width: 100%;" src="{{asset('images/meserii/bucatari3.jpg')}}" class="img-thumbnail">
     </div>
-    <div class="col-md-12">
-        <p>Curriculum modular a fost eleborat in stransa colaborare cu reprezentantii mediului de afaceri.
-    Cadrele didactice și-au perfecționat competențele profesionale prin cursuri specializate,
-    intership-uri, cursuri de limba engleza și formari psihopedagogice.
-școala beneficiaza de un laborator modern dotat cu echipamente tehnice performante.
-Acest laborator este o șansa pentru elevi de a se pregati pentru piața muncii, destul de
-competitiva, sa-și capete competențele necesare pentru a avea un loc de munca bine platit.
-</p>
 </div>
-    </div>
+</div>
 @endsection
