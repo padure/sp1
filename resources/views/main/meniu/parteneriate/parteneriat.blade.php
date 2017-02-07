3dd2b8fa326113979da99774e7ec29aceadbdffd
valintin <valintin@192.168.56.1>
2017-02-07 23:37:37 +0200

    parteneriate din baza de date

--- resources/views/main/meniu/parteneriate/parteneriat.blade.php
+++ resources/views/main/meniu/parteneriate/parteneriat.blade.php
@@ -1,30 +1,25 @@
 @extends("main.base")
 @section("content")
-<h3>Parteneriate</h3>
-<div class="col-md-12">
-    <div class="col-md-3 parteneri-all">
-        <a href="#"><img src="{{ asset('allimages/parteneriati/170207052016.png')}}"></a>
-    </div>
-    <div class="col-md-3 parteneri-all">
-        <a href="#"><img src="{{ asset('allimages/parteneriati/170207052016.png')}}"></a>
-    </div>
-    <div class="col-md-3 parteneri-all">
-        <a href="#"><img src="{{ asset('allimages/parteneriati/170207052016.png')}}"></a>
-    </div>
-    <div class="col-md-3 parteneri-all">
-        <a href="#"><img src="{{ asset('allimages/parteneriati/170207052016.png')}}"></a>
-    </div>
-    <div class="col-md-3 parteneri-all">
-        <a href="#"><img src="{{ asset('allimages/parteneriati/170207052016.png')}}"></a>
-    </div>
-    <div class="col-md-3 parteneri-all">
-        <a href="#"><img src="{{ asset('allimages/parteneriati/170207052016.png')}}"></a>
-    </div>
-    <div class="col-md-3 parteneri-all">
-        <a href="#"><img src="{{ asset('allimages/parteneriati/170207052016.png')}}"></a>
-    </div>
-    <div class="col-md-3 parteneri-all">
-        <a href="#"><img src="{{ asset('allimages/parteneriati/170207052016.png')}}"></a>
-    </div>
+<div class="col-md-12 whiteclass" style="padding-bottom: 20px;">
+    <h3>
+        @if($post["den"]==0)
+            Parteneri educaționali
+        @elseif($post["den"]==1)
+            Parteneri naționali
+        @else
+            Parteneri internaționali
+        @endif
+    </h3>
+    @if(!empty($post["parteneriate"]) && count($post["parteneriate"])>0)
+        @foreach($post["parteneriate"] as $i)
+            <div class="col-md-3 parteneri-all">
+                <a href="{{$i->link}}" target="_blank">
+                    <img src="{{ asset($i->image)}}">
+                </a>
+            </div>
+        @endforeach
+    @else
+        <h1>Nu sunt parteneriati</h1>
+    @endif
 </div>
 @endsection
