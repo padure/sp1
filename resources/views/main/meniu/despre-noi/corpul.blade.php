d84234d2af7e722af2a1538bf1b9228d05c1a885
valintin <valintin@192.168.56.1>
2017-02-07 23:39:39 +0200

    corp

--- resources/views/main/meniu/despre-noi/corpul.blade.php
+++ resources/views/main/meniu/despre-noi/corpul.blade.php
@@ -1,24 +1,24 @@
 @extends("main.base")
 @section("content")
+<div style="padding: 10px" class="whiteclass">
+    <h3>Corpul didactic</h3>
     @if(!empty($post) && count($post)>0)
-        <div style="padding: 10px" class="whiteclass">
-            <h3>Corpul didactic</h3>
-            <table class="table table-striped" >
+        <table class="table table-striped" >
+            <tr>
+                <th>NR/Ord</th>
+                <th>Nume</th>
+                <th>Prenume</th>
+                <th>Functia</th>
+            </tr>
+            @for($i=0;$i< count($post);$i++)
                 <tr>
-                    <th>NR/Ord</th>
-                    <th>Nume</th>
-                    <th>Prenume</th>
-                    <th>Functia</th>
+                    <td>{{$i+1}}</td>
+                    <td>{{$post[$i]->nume}}</td>
+                    <td>{{$post[$i]->prenume}}</td>
+                    <td>{{$post[$i]->functia}}</td>
                 </tr>
-                @for($i=0;$i< count($post);$i++)
-                    <tr>
-                        <td>{{$i+1}}</td>
-                        <td>{{$post[$i]->nume}}</td>
-                        <td>{{$post[$i]->prenume}}</td>
-                        <td>{{$post[$i]->functia}}</td>
-                    </tr>
-                @endfor
-            </table>
-        </div>
+            @endfor
+        </table>
     @endif
+    </div>
 @endsection
