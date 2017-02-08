48fd748e57ed1f78eba00a4effa6ee8ba71f3cb9
Padure Gheorghe <Padure Gheorghe@PC-Gheorghe>
2017-02-08 13:59:20 +0200

    Merge origin/master
    Conflicts:
    	resources/views/main/meniu/parteneriate/parteneriat.blade.php

--- resources/views/main/meniu/contacte/contact.blade.php
+++ resources/views/main/meniu/contacte/contact.blade.php
@@ -14,11 +14,8 @@
     <div class="col-lg-4">
         <img src="{{ asset ( "images/scoala.jpg" ) }}" class="img-thumbnail">
     </div>
-<iframe
-  width="100%"
-  height="450"
-  frameborder="0" style="border:0"
-  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBHkMbZ818uHMYUZELEJ1ZwGcqLRw--BjU
-    &q=Space+Needle,Seattle+WA" allowfullscreen>
-</iframe>
+    <hr>
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2000.772120035712!2d28.189923690590508!3d45.89602195597536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sro!2s!4v1486555049567" width="100%" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+</div>
 @endsection
