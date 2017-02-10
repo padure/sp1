4e1ea15b3656d84107cd151c70a6fde39588be9b
valintin <valintin@192.168.56.1>
2017-02-10 22:12:11 +0200

    base cu vizualizari si meniu

--- resources/views/main/base.blade.php
+++ resources/views/main/base.blade.php
@@ -57,7 +57,7 @@
 		<!-- menu top-->
             <div class="row my-nave">
 		<nav class="navbar navbar-inverse" style=" margin-bottom: 0px;">
-			  <div class="container-fluid " style="background-color: #4668fd;!important">
+			  <div class="container-fluid " style="background-color: #4668fd !important">
 				<div class="navbar-header">
 				  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
 					<span class="icon-bar"></span>
